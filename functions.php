89359a2db647ec4656881c466cc8e528105f5ab9
Weston Wedding <wes@wawco.com>
2015-04-20 23:04:07 -0700

    Removed explicit calls to wp_title() from the header to rely on the title feature for themes.

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 function register_wawco_tuxedo_menus() {
-  register_nav_menus(
-      array(
-        'main-nav' => __( 'Main Nav Menu' )
-      )
-  );
+  register_nav_menus( array(
+      'main-nav' => __( 'Main Nav Menu' )
+  ) );
 }
-add_action( 'init', 'register_wawco_tuxedo_menus' );
+add_action('init', 'register_wawco_tuxedo_menus');
+
+add_theme_support( 'title-tag' );
--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
 		<?php wp_head(); ?>
 	</head>
