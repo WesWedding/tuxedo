bf1244c44efbde147746b9bbd65bb9d7b40069ef
Weston Wedding <wes@wawco.com>
2017-03-23 21:33:43 -0700

    Some typography adjustments, some content margins in, a little bit of color added to the mix.

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|PT+Serif" rel="stylesheet">
 	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
 	<?php wp_head(); ?>
 </head>
