c5eda60554a990acc2a15c6ff016f780e153a1e8
Weston Wedding <wes@wawco.com>
2018-01-22 21:41:23 -0800

    Adjusting some styles a little bit, and also cleaning up the generic page template.

--- page.php
+++ page.php
@@ -8,12 +8,8 @@
  */
 
 get_header(); ?>
-
-<p class="debug">File: page.php</p>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<h1><?php bloginfo( 'name' ); ?></h1>
-			<h2><?php bloginfo( 'description' ); ?></h2>
 			<?php get_template_part('template-parts/loop'); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
