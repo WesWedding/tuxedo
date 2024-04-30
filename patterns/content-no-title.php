05dca8950e308ab788543656ccb2f3a5d67e80a6
Weston Wedding <wes@wawco.com>
2024-04-30 12:31:53 -0700

    Human readability improvement; divs are aligned, wp tags are aligned.

--- patterns/content-no-title.php
+++ patterns/content-no-title.php
@@ -11,7 +11,11 @@
 <!-- wp:post-featured-image {"isLink":true} /-->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","flexWrap":"nowrap"},"fontSize":"small"} -->
-<div class="wp-block-group has-small-font-size"><!-- wp:html --> <div><?php echo esc_html_x( 'Posted on &nbsp;', 'Label for the date of a post', 'wwsuit' ); ?></div><!-- /wp:html --><!-- wp:post-date {"format":"M j, Y"} /--></div>
+<div class="wp-block-group has-small-font-size">
+	<!-- wp:html -->
+	<div><?php echo esc_html_x( 'Posted on &nbsp;', 'Label for the date of a post', 'wwsuit' ); ?></div>
+	<!-- /wp:html --><!-- wp:post-date {"format":"M j, Y"} /-->
+</div>
 <!-- /wp:group -->
 
 <!-- wp:separator {"backgroundColor":"white"} -->
@@ -21,13 +25,10 @@
 <!-- wp:post-content /-->
 
 <!-- wp:group {"style":{"typography":{"fontSize":"14px"}},"layout":{"type":"flex"}} -->
-<div class="wp-block-group" style="font-size:14px"><!-- wp:post-author {"showAvatar":false,"showBio":false} /-->
-
-	<!-- wp:post-date {"isLink":true} /-->
-
-	<!-- wp:post-terms {"term":"category"} /-->
-
-	<!-- wp:post-terms {"term":"post_tag"} /--></div>
+<div class="wp-block-group has-small-font-size" style="font-size:14px"><!-- wp:post-date {"format":"Y-m-d"} /-->
+	<!-- wp:post-terms {"term":"category","prefix":"Filed in: "} /-->
+	<!-- wp:post-terms {"term":"post_tag","prefix":"Tagged: "} /-->
+</div>
 <!-- /wp:group -->
 
 <!-- wp:spacer {"height":"40px"} -->
